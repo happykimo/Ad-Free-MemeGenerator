8cea91543ddfd35e1ff261bfea6292b56f2fd598
Alex Chow <alex0091845@gmail.com>
2020-02-18 11:40:22 -0800

    Get meme impl done. Needs refactoring

--- app/src/main/java/org/tritonhacks/memegenerator/GetMemeActivity.java
+++ app/src/main/java/org/tritonhacks/memegenerator/GetMemeActivity.java
@@ -1,7 +1,9 @@
 package org.tritonhacks.memegenerator;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,45 +11,120 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.squareup.picasso.Picasso;
-
-import static org.tritonhacks.memegenerator.MainActivity.URL;
 
 public class GetMemeActivity extends AppCompatActivity {
 
-    private static RandomMeme randomMeme;
+    final static String URL = "https://meme-api.herokuapp.com/gimme/cleanmemes/100";
+
+    private static JsonObject randomMeme;
+    private static JsonArray memeList;
+    private ImageView imgV_Meme;
+    private String memeUrl;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_meme);
 
-        testRequest();
-
-
+        initViews();
+        getMeme();
     }
 
-    // A simple request; will need another implementation through a singleton
-    // class.
-    private void testRequest() {
-        Gson gson = new Gson();
+    /**
+     * Initializes all necessary views.
+     */
+    private void initViews() {
+        final Button getAnotherMeme = findViewById(R.id.btn_get_another_meme);
+        getAnotherMeme.setOnClickListener(v -> getMeme());
+        imgV_Meme = findViewById(R.id.imgV_meme);
+    }
+
+    /**
+     * Get a meme either through request or load the meme if list is not empty.
+     */
+    private void getMeme() {
+        if(isMemeListEmpty()) {
+            getRequest();
+        } else {
+            loadMeme();
+        }
+    }
+
+    /**
+     * Saves the current state. Namely the URL of the current meme.
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("url", memeUrl);
+        super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Restores the previous state, with URL of the previously viewed meme.
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        memeUrl = savedInstanceState.getString("url");
+        Picasso.with(this).load(memeUrl).into(imgV_Meme);
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Helper method to load the meme image onto ImageView
+     */
+    private void loadMeme() {
+        memeUrl = popMeme();
+        Picasso.with(this).load(memeUrl).into(imgV_Meme);
+    }
+
+    /**
+     * Make a GET request to the memes by the URL string.
+     */
+    private void getRequest() {
         RequestQueue queue = Volley.newRequestQueue(this);
 
         // Request a string response the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
                 response -> {
 
-                    randomMeme = gson.fromJson(response, RandomMeme.class);
-                    String url = randomMeme.getUrl();
-                    ImageView imgV_Meme = findViewById(R.id.imgV_meme);
-                    Picasso.with(this).load(url).into(imgV_Meme);
+                    randomMeme = (JsonObject) JsonParser.parseString(response);
+                    memeList = randomMeme.getAsJsonArray("memes");
+                    loadMeme();
 
-                }, error -> System.out.println("Error!"));
+                }, error -> {
+            // make a Toast message to show error. Needs to be polished.
+            // TODO
+            Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show();
+        });
 
         queue.add(stringRequest);
 
         // Add the request to the RequestQueue. NOT DONE -Alex
         // SingletonRQ.getInstance(this).addToRequestQueue(jsonObjRequest);
+    }
+
+    /**
+     * Removes the last meme object in the array and gets its url.
+     * @return url as a String
+     */
+    private String popMeme() {
+        JsonElement meme = memeList.remove(memeList.size() - 1);
+        JsonElement jsonUrl = ((JsonObject)meme).get("url");
+        return jsonUrl.getAsString();
+    }
+
+    /**
+     * Checks if memeList is null or empty. SHORT-CIRCUITING
+     * @return true if memeList is empty, false otherwise
+     */
+    private boolean isMemeListEmpty() {
+        return memeList == null || memeList.size() == 0;
     }
 }
--- app/src/main/java/org/tritonhacks/memegenerator/MainActivity.java
+++ app/src/main/java/org/tritonhacks/memegenerator/MainActivity.java
@@ -3,34 +3,10 @@ package org.tritonhacks.memegenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.squareup.picasso.Picasso;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
-    // used in GetMemeActivity; move it there
-    final static String URL = "https://meme-api.herokuapp.com/gimme";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
